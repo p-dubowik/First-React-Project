92239ac256b8a9f03837b3ff951ab92023d443a0
Paweł Dubowik <p.dubowik27@gmail.com>
2026-01-17 19:50:05 +0100

    Add 'Add column' functionality

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,24 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import ColumnForm from '../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
+
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    
+
+    const addColumn = newColumn => {
+            setColumns([...columns, { id: shortid(), title: newColumn.title, icon:newColumn.icon }]);
+    };
+
+
     return (
         <div className={styles.list}>
             <header className={styles.header}>
@@ -11,11 +28,13 @@ const List = () => {
                 <p className = {styles.description}>Interesting things I want to check out</p>
             </header>
 
+            {/* columns section */}
             <section className={styles.columns}>
-                <Column title='Books' icon='book' />
-                <Column title='Movies' icon='film' />
-                <Column title='Games' icon='gamepad' />
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+
+            {/* basic form */}
+            <ColumnForm action={addColumn} />
 
         </div>
     );
