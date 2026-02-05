abefd1b2a3b05bd1de52e058d144d705cf8e0b29
Paweł Dubowik <p.dubowik27@gmail.com>
2026-02-06 00:38:25 +0100

    Change link appearance while active. HIDE SCROLLBAR (global::scrollbar)

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -9,9 +10,18 @@ const NavBar = () => {
                 </a>
 
                 <ul>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/favorite'>Favorite</a></li>
-                    <li><a href='/about'>About</a></li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/favorite">Favorite</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/about">About</NavLink>
+                    </li>
                 </ul>
             </div>
         </nav>
